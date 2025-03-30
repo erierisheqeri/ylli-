56dae654b952763662a51c6cbe0f4c066a211850
erierisheqeri <haivanaofficial@gmail.com>
2025-03-30 19:40:36 +0200

    Add files via upload

---
+++ replit.nix
@@ -0,0 +1,9 @@
+
+{ pkgs }: {
+    deps = [
+        pkgs.nodejs-18_x
+        pkgs.ffmpeg
+        pkgs.python3
+        pkgs.git
+    ];
+}
---
+++ settings.js
@@ -0,0 +1,13 @@
+const settings = {
+  packname: '⭐',
+  author: '⭐',
+  botName: "⭐",
+  botOwner: 'eri!', // Your name
+  ownerNumber: '447878935454', //Your number
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "YLLI-MD.",
+  version: "0.6.9",
+};
+
+module.exports = settings;
